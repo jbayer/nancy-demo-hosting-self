84d66f6c52857bfb37848e82ccdafe2cf4845575
James Bayer <jbayer@gopivotal.com>
2013-11-13 14:51:44 -0800

    read hostname and domain from env variables

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Nancy.Demo.Hosting.Self
+namespace Nancy.Demo.Hosting.Self
 {
     using System;
     using System.Diagnostics;
@@ -9,13 +9,14 @@
     {
         static void Main()
         {
-            var hostname = "deep-moon-1452";
+            var hostname = "System.Environment.GetEnvironmentVariable("HOSTNAME");";
+            var domain = "System.Environment.GetEnvironmentVariable("DOMAIN");";
 
             var port = System.Environment.GetEnvironmentVariable("PORT");
-            var nancyHost = new NancyHost(new Uri("http://" + hostname + ".herokuapp.com:" + port), new Uri("http://0.0.0.0:" + port));
+            var nancyHost = new NancyHost(new Uri("http://" + hostname + "." + domain + ":" + port), new Uri("http://0.0.0.0:" + port));
             nancyHost.Start();
 
-            Console.WriteLine("Nancy now listening - navigating to http://" + hostname +".herokuapp.com:" + port +".");
+            Console.WriteLine("Nancy now listening - navigating to http://" + hostname +"." + domain + ":" + port +".");
 
             var line = Console.ReadLine();
             while(line != "quit") {
